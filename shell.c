2c7c53c9b6a64e8ac8da6fe781378be22ef4e9e4
Xi <xidulu@gmail.com>
2018-03-12 22:37:59 +0800

    proj1 finished

--- shell.c
+++ shell.c
@@ -3,22 +3,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <wordexp.h>
+#include <fcntl.h>
+
 #define MAXLEN 100
 #define CMDMAX 100
+#define HISTORYMAX 100
 
+char history[MAXLEN][HISTORYMAX];
+int recordNum = 0;
 
-char** lineSplit(char* line) {
-    // FIXME
-    return NULL;
-}
-
-int changeInput(char* file) {
-
-}
-
-int changeOutput(char* file) {
-
-}
+int background = 0;
 
 int changeDirectory(char** args) {
     if (args[1] == NULL) {
@@ -32,105 +26,183 @@ int changeDirectory(char** args) {
     return 0;
 }
 
-/**
- * args:argument list
- * background: 1 for running background, 0 otherwise
- * redirect= 0:output to file. 1:concatenate to file. 2:for input from file
- * FILE: string for filename
- * */
-int launchProgram(char** args, int background,
-                    int redirect, char* file) {
-    int status;
-    pid_t pid = fork();
-    if (pid == 0)
+// for debug usage
+void printArgs(char** args) {
+    int  i;
+    for(i = 0; i < 5; i++) {
+        printf("%s ", args[i]);
+    }
+    printf("\n");
+}
+
+int executeProcess(char** args, int in, int out) {
+    if (strcmp(args[0], "cd") == 0) {
+        return changeDirectory(args);
+    }
+    // printArgs(args);
+    pid_t pid;
+    if ((pid = fork()) == 0)
     {
-        if (redirect == 0) freopen(file, "w", stdout);
-        if (redirect == 1) freopen(file, "at", stdout);
-        if (redirect == 2) freopen(file, "rt", stdin);
-        if(execvp(args[0], args) < 0) {
-            printf("execute error!\n");
+        if (in != 0) {
+            dup2(in, 0);
+            close(in);
+        }
+        if (out != 1) {
+            dup2(out, 1);
+            close(out);
+        }
+        if (execvp(args[0], args) < 0) {
+            printf("Execution error!\n");
+            exit(-1);
         }
         return 0;
-    }
-    else if (!background) {
+    } else if (!background){
         wait(NULL);
-    } 
-    return 0;
-}  
+    }
+    return pid;
+}
+
 
 int printCurrentDirectory() {
     char cwd[1024];
     if (getcwd(cwd, sizeof(cwd)) != NULL)
-        fprintf(stdout, "Current working dir: %s\n", cwd);
+        fprintf(stdout, "%s", cwd);
     else
         perror("getcwd() error");
     return 0;
 }
 
-/**
- * Unit test for cd command (kind of)
- * */
-int testCdCommand() {
-    printCurrentDirectory();
-    char* args[2];
-    strcpy(args[0], "cd");
-    strcpy(args[1], "..");
-    changeDirectory(args);
-    printCurrentDirectory();
+
+int str2num(char* str) {
+    int len = strlen(str);
+    int result = 0;
+    int i;
+    for(i = 0; i < len; i++) {
+        result = result * 10 + str[i] - '0';
+    }
+    return result;
 }
+
+/**
+ * Parse a line.
+ * Read tokens one by one, from left to right.
+ **/
+int parseLine(char* buffer) {
+    char *args[CMDMAX + 1] = {0};
+    int n_cmds = 0;
+    int redirect = -1;
+    int in = 0;
+    int out = 1;
+    
+    char *nextToken;
+    char *file;
+    args[0] = strtok(buffer," ");
+    if (args[0] == NULL) {
+        return 0;
+    }
+    if (strcmp(args[0], "exit") == 0) {
+        exit(0);
+    }
+    if (strcmp(args[0], "history") == 0) {
+        char* numStr;
+        if ((numStr  = strtok(NULL, " ")) == NULL) {
+            int i;
+            for(i = 0; i < recordNum - 1; i++) {
+                printf("%s", history[i]);
+            }
+        } else {
+            int numVal = str2num(numStr);
+            int i;
+            if (numVal > recordNum - 1) {
+                printf("Out of range!\n");
+                return -1;
+            }
+            for(i = recordNum - 1 - numVal;
+                i < recordNum - 1; i++) {
+                    printf("%s", history[i]);
+                }
+        }
+        return 0;
+    }
+    while(1) {
+        // end of a line.
+        if ((nextToken = strtok(NULL, " ")) == NULL) {
+            // no token before EOL
+            if (n_cmds == -1) {
+                break;
+            }
+            args[++n_cmds] = NULL;
+            executeProcess(args, in, out);
+            break;
+        }
+        if (strcmp(nextToken, "&") == 0) {
+            background = 1;
+            continue;
+        }
+        if (strcmp(nextToken, "|") == 0) {
+            int fd[2];
+            pipe(fd);
+            args[++n_cmds] = NULL;
+            if (executeProcess(args, in, fd[1]) < 0) {
+                break;
+            }
+            close(fd[1]);
+            // input source for next process.
+            in = fd[0];
+            n_cmds = -1;
+            background = 0;
+            continue;
+        }
+        if (strcmp(nextToken, ">") == 0) {
+            args[++n_cmds] = NULL;
+            file = strtok(NULL, " ");
+            int fd = open(file, O_RDWR | O_CREAT);
+            if (executeProcess(args, in, fd) < 0) {
+                break;
+            }
+            close(fd);
+            n_cmds = -1;
+            background = 0;
+            continue;
+        }
+        if (strcmp(nextToken, ">>") == 0) {
+            args[++n_cmds] = NULL;
+            file = strtok(NULL, " ");
+            int fd = open(file, O_RDWR | O_CREAT | O_APPEND);
+            if (executeProcess(args, in, fd) < 0) {
+                break;
+            }
+            close(fd);
+            n_cmds = -1;
+            continue;
+        }
+        if (strcmp(nextToken, "<") == 0) {
+            args[++n_cmds] = NULL;
+            file = strtok(NULL, " ");
+            int fd = open(file, O_RDWR);
+            if (executeProcess(args, fd, out) < 0) {
+                break;
+            }
+            close(fd);
+            n_cmds = -1;
+            continue;
+        }
+        args[++n_cmds] = nextToken;
+    }
+    return 0;
+}
+
 
 int main() {
     while(1) {
-        printf("shell#");
+        fflush(stdout);
+        printCurrentDirectory();
+        printf(":");
         char buffer[MAXLEN];
         fgets(buffer, MAXLEN, stdin);
+        strcpy(history[recordNum++], buffer);
         buffer[strcspn(buffer, "\n")] = 0;
-        char *args[CMDMAX + 1] = {0};
-        int n_cmds = 0;
-        int background = 0;
-        int redirect = -1;
-        char *nextToken;
-        char* file;
-        args[0] = strtok(buffer," ");
-        while ((nextToken = strtok(NULL, " ")) != NULL)
-        {
-            if (redirect != -1) {
-                n_cmds++;
-                file = nextToken;
-                continue;
-            }
-            if (strcmp(nextToken, "&") == 0) {
-                background = 1;
-                continue;
-            }
-            if (strcmp(nextToken, ">") == 0) {
-                n_cmds++;
-                redirect = 0;
-                continue;
-            }
-            if (strcmp(nextToken, ">>") == 0) {
-                n_cmds++;
-                redirect = 1;
-                continue;
-            }
-            if (strcmp(nextToken, "<") == 0) {
-                n_cmds++;
-                redirect = 2;
-                continue;
-            }
-            n_cmds++;
-            args[n_cmds] = nextToken;
-        }
-        args[n_cmds + 1] = NULL;
-        if (strcmp(args[0], "cd") == 0) {
-            int t = changeDirectory(args);
-            continue;
-        }
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        }
-        launchProgram(args, background,
-                redirect, file);
+        parseLine(buffer);
     }
     return 0;
 }
